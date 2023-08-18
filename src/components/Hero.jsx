2614326e6570ae61054cc3897ed1ea69c862b6b5
Shreyas S N <shreyassn91224@gmail.com>
2023-08-19 01:31:16 +0530

    update landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Shreyas</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop ML Models, user
+            interfaces, <br className='sm:block hidden' /> web applications and what not
           </p>
         </div>
       </div>
